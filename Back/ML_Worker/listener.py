82421a44ee1739f18c9f873b34b6372178ad467a
komtriangle <komlevdanila742@gmail.com>
2023-01-21 18:06:16 +0300

    env ml

--- Back/ML_Worker/listener.py
+++ Back/ML_Worker/listener.py
@@ -3,11 +3,19 @@ import sys
 import pika
 from predictor import Predictor, ModelInputs
 from pathlib import Path
+import os
+
+host_name = os.environ.get("RABBITMQ.HOSTNAME")
+virtual_host = os.environ.get("RABBITMQ.VIRUALHOST")
+user_name = os.environ.get("RABBITMQ.USERNAME")
+password = os.environ.get("RABBITMQ.PASSWORD")
+predict_queue = os.environ.get("RABBITMQ.PREDICTQUEUE")
+
 
 
 if __name__ == '__main__':
-    credentials = pika.PlainCredentials('xmasuser','xmaspassword')
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='62.84.127.116', port='5672', credentials= credentials))
+    credentials = pika.PlainCredentials(user_name, password)
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=host_name, port='5672', credentials= credentials))
     channel = connection.channel()
     pred = Predictor(Path('./model'))
 
@@ -20,5 +28,5 @@ if __name__ == '__main__':
         except Exception as e:
             print(e, file=sys.stderr)
     print('started listening guys')
-    channel.basic_consume(queue='predict', on_message_callback=callbackFunctionForQueueA, auto_ack=True)
+    channel.basic_consume(queue= predict_queue, on_message_callback=callbackFunctionForQueueA, auto_ack=True)
     channel.start_consuming()
